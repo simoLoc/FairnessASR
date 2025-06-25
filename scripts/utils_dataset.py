c6af58da03dd383cceff06483c0683e61d05f2bc
simoLoc <simonaloc1@yahoo.it>
2025-06-25 17:43:42 +0200

    Dataset e modello wav2vec2

---
+++ scripts/utils_dataset.py
@@ -0,0 +1,29 @@
+from datasets import load_dataset
+from huggingface_hub import login
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+import torch
+import torchaudio
+
+def get_dataset():
+    # Login to Hugging Face Hub
+    login("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")
+
+    # Load the dataset
+    ds = load_dataset("SALT-NLP/spotify_podcast_ASR")
+
+    data = ds['train']
+
+    return data
+
+
+def print_dataset_info(data):
+    # Numero elementi del dataset
+    num_elements = len(data)
+    print(f"Numero di elementi nel dataset: {num_elements}")
+
+    # Mostra i primi 3 esempi 
+    for i in range(3):
+        print(f"\nEsempio {i+1}:")
+        print(data[i])    
+
+
